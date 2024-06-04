eba026258213b8510a9322a75308b0a469884a9d
JohnHenin <127199730+JohnHenin@users.noreply.github.com>
2024-06-04 17:32:16 +0530

    Opencv project

--- add_faces.py
+++ add_faces.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+output_dir='output_images'
+os.makedirs(output_dir, exist_ok=True)
 
 faces_data=[]
 
@@ -16,10 +18,13 @@ while True:
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces=facedetect.detectMultiScale(gray, 1.3 ,5)
     for (x,y,w,h) in faces:
-        crop_img=frame[y:y+h, x:x+w,:]
+        crop_img=frame[y:y+h, x:x+w, :]
         resized_img=cv2.resize(crop_img, (50,50))
         if len(faces_data)<=100 and i%10==0:
             faces_data.append(resized_img)
+            img_name = os.path.join(output_dir, "opencv_frame_{}.png".format(i))
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
         i=i+1
         cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 1)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
